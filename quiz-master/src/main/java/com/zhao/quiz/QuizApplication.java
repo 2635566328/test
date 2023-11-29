ff6c3886b66addf999df6acc4536ca02eefb5fc3
2635566328 <2635566328@qq.com>
2023-11-29 23:19:16 +0800

    源代码

---
+++ quiz-master/src/main/java/com/zhao/quiz/QuizApplication.java
@@ -0,0 +1,16 @@
+package com.zhao.quiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *  慕代码 www.mudaima.com - 打造海量优质源码服务平台
+ */
+@SpringBootApplication
+public class QuizApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuizApplication.class, args);
+    }
+
+}
